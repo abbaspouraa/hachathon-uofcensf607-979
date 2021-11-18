18f355c14d28709816c1e4d8f6d4661d6c1ba32e
Amir Abbaspour <83620288+abbaspouraa@users.noreply.github.com>
2021-11-18 01:12:13 -0700

    swagger and mySQL added

---
+++ JavaProject/src/main/java/com/ENSF607/AnimalProject/repository/UserRepository.java
@@ -0,0 +1,23 @@
+package com.ENSF607.AnimalProject.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UserRepository {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<String> getAllUserNames() {
+        List<String> usernameList = new ArrayList<>();
+        usernameList.addAll(
+                jdbcTemplate.queryForList("select name from user;", String.class)
+        );
+        return usernameList;
+    }
+}
