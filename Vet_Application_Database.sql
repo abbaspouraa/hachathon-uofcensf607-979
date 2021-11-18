86c35fd1b7827daf2476e6607d7a392b10325f6d
Ali Zirahi <ali.zirahi@ucalgary.ca>
2021-11-18 02:01:36 -0700

    Create Vet_Application_Database.sql

---
+++ Vet_Application_Database.sql
@@ -0,0 +1,87 @@
+DROP DATABASE IF EXISTS Vet_Application;
+CREATE DATABASE Vet_Application; 
+USE Vet_Application;
+
+DROP TABLE IF EXISTS ANIMAL;
+CREATE TABLE ANIMAL (
+	AnimalId				integer not null,
+	Name					varchar(25) not null,
+	Species					varchar(25) not null,
+    Sex						char(1) not null,
+    Breed					varchar(25) not null,
+    Age						integer not null,
+    RFID					varchar(25) not null,
+    Altered					varchar(25) not null,
+    Weight					integer not null,
+    Special_Problem			varchar(200),
+    Continuous_Medication	varchar(100),
+    Special_Instractions	varchar(200),
+    Special_Diet			varchar(200),
+    Tatoo					varchar(100) not null,
+    Color					varchar(25) not null,
+    Status					varchar(50) not null,
+    
+	primary key (AnimalId,Status)
+);
+
+DROP TABLE IF EXISTS USER;
+CREATE TABLE USER (
+	UserId					integer not null,
+    FName					varchar(25) not null,
+    LName					varchar(25) not null,
+    Email					varchar(200) not null,
+    Role					varchar(50) not null,
+    
+    primary key (UserId)
+);
+
+DROP TABLE IF EXISTS COMMENT;
+CREATE TABLE COMMENT (
+	CmntId					integer not null,
+    Date					Date,
+    UserId					integer not null,
+    AnimalId				integer not null,
+    Note					varchar(2000) not null,
+    
+    primary key (AnimalId,CmntId),
+    foreign key (AnimalId) references ANIMAL(AnimalId),
+    foreign key (UserId) references USER(UserId)
+);
+
+DROP TABLE IF EXISTS ONGOIN_CARE;
+CREATE TABLE ONGOIN_CARE (
+	CareId					integer not null,
+	UserId					integer not null,
+    AnimalId				integer not null,
+    Date					Date not null,
+    ProcessDescription		varchar(300) not null,
+    Weight					integer,
+    Drug					varchar(1000) not null,
+    NextDue					Date,
+    Note					varchar(2000) not null,
+    
+    primary key (AnimalId,CareId),
+    foreign key (AnimalId) references ANIMAL(AnimalId),
+    foreign key (UserId) references USER(UserId)
+);
+
+DROP TABLE IF EXISTS ANIMAL_STATUS;
+CREATE TABLE ANIMAL_STATUS (
+	StatusId				integer not null,
+	CareAttId				integer not null,
+    AnimalId				integer not null,
+    Date					Date not null,
+    ProcessDescription		varchar(300) not null,
+    Temperature				integer,
+    Weight					integer,
+    HeartRate				integer,
+    Symptoms				varchar(1000) not null,
+    DiagnoseDrug			varchar(1000) not null,
+    VetId					integer not null,
+    
+    primary key (AnimalId,StatusId),
+    foreign key (AnimalId) references ANIMAL(AnimalId),
+    foreign key (CareAttId) references USER(UserId),
+    foreign key (VetId) references USER(UserId)    
+);
+
